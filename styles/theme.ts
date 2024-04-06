a1897751b78314ee76764306a241cb941b93211d
cupoftea4 <nadiia.telko.pz.2021@lpnu.ua>
2024-04-06 22:29:45 +0300

    feat: basic ui components

---
+++ styles/theme.ts
@@ -0,0 +1,51 @@
+
+type Theme = {
+  background: string;
+  foreground: string;
+  /**
+   * Text color for foreground elements (e.g. button text)
+   */
+  fgText: string;
+  text: string;
+  bgCircle: string;
+  modalBg: string;
+  backdrop: string;
+  /**
+  * List items, recipe list filter button, etc.
+  */
+  cardBg: string;
+  stepDone: string;
+  stepUndone: string;
+  searchBarBg: string;
+};
+
+export const lightTheme: Theme = {
+  background: 'white',
+  foreground: '#181818',
+  fgText: '#F3F3F3',
+  text: '#181818',
+  bgCircle: 'rgba(147, 190, 103, 0.12)',
+  modalBg: '#F3F3F3',
+  backdrop: 'rgba(32, 32, 32, 0.23)',
+  cardBg: '#FFFFFF',
+  stepDone: '#3D974B',
+  stepUndone: '#D5D5D5',
+
+  searchBarBg: '#F3F3F3',
+};
+
+export const darkTheme: Theme = {
+  background: '#151515',
+  foreground: '#F3F3F3',
+  fgText: '#181818',
+  text: '#F3F3F3',
+  bgCircle: 'rgba(37, 44, 34, 0.34)',
+  modalBg: '#181818',
+  backdrop: 'rgba(0, 0, 0, 0.27)',
+  cardBg: '#232323',
+  stepDone: '#3D974B',
+  stepUndone: '#6B6B6B',
+
+
+  searchBarBg: '#F3F3F3',
+};
